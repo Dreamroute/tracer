df19f33e5a00e34ed245bdf829c1cdca61f99629
w.dehai <w.dehai>
2021-08-23 11:49:04 +0800

    init.

---
+++ tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/AA.java
@@ -0,0 +1,7 @@
+package com.github.dreamroute.tracer.starter;
+
+/**
+ * @author w.dehai.2021/8/23.10:04
+ */
+public class AA {
+}
---
+++ tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/config/TracerProperties.java
@@ -0,0 +1,10 @@
+package com.github.dreamroute.tracer.starter.config;
+
+import lombok.Data;
+
+/**
+ * @author w.dehai.2021/8/23.10:10
+ */
+@Data
+public class TracerProperties {
+}
