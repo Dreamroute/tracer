667501a646181e0c4e0040ff0aca1b71aca54155
w.dehai <w.dehai>
2021-09-28 10:29:58 +0800

    doc: 参数序列化异常处理

--- tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/ArgsPrinterAutoConfiguration.java
+++ tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/ArgsPrinterAutoConfiguration.java
@@ -11,7 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.Resource;
+import java.io.Serializable;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.alibaba.fastjson.JSON.toJSONString;
@@ -42,7 +44,15 @@ public class ArgsPrinterAutoConfiguration {
         MethodInterceptor interceptor = invocation -> {
             String methodName = invocation.getMethod().getDeclaringClass().getName() + "." + invocation.getMethod().getName();
             Object[] args = invocation.getArguments();
-            log.info("方法: {}, 参数: {}", methodName, toJSONString(args));
+            if (args != null && args.length > 0) {
+                List<Object> collect = Arrays.stream(args).filter(arg -> arg instanceof Serializable).collect(Collectors.toList());
+                try {
+                    log.info("方法: {}, 参数: {}", methodName, toJSONString(collect));
+                } catch (Exception e) {
+                    log.error("此处参数序列化失败了，已经经过特殊处理，不会影响业务，开发人员可以尝试排查一下此处的错误原因，方法名: {}, 异常: {}", methodName, e);
+                }
+
+            }
             return invocation.proceed();
         };
         return new DefaultPointcutAdvisor(pointcut, interceptor);
