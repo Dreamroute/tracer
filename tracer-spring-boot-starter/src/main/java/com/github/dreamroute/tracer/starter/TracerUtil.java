d75040a81edb62416edb3a46ed031486a39da18c
wangdehai <wangdehai@yzw.cn>
2022-08-10 13:53:20 +0800

    feat: 支持多线程

--- tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/TracerUtil.java
+++ tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/TracerUtil.java
@@ -1,8 +1,15 @@
 package com.github.dreamroute.tracer.starter;
 
-import org.slf4j.MDC;
+import com.alibaba.ttl.TransmittableThreadLocal;
+import org.slf4j.spi.MDCAdapter;
 
+import static cn.hutool.core.util.ReflectUtil.getFieldValue;
+import static cn.hutool.core.util.ReflectUtil.setFieldValue;
 import static com.github.dreamroute.tracer.starter.TracerProperties.TRACE_ID;
+import static org.slf4j.MDC.get;
+import static org.slf4j.MDC.getMDCAdapter;
+import static org.slf4j.MDC.put;
+import static org.slf4j.MDC.remove;
 
 /**
  * trace id工具类，用于包装SLF4J的MDC
@@ -12,15 +19,22 @@ import static com.github.dreamroute.tracer.starter.TracerProperties.TRACE_ID;
 public class TracerUtil {
     private TracerUtil() {}
 
+    private static final String NAME = "copyOnThreadLocal";
+
     public static void setTraceId(String traceId) {
-        MDC.put(TRACE_ID, traceId);
+        MDCAdapter mdcAdapter = getMDCAdapter();
+        Object tl = getFieldValue(mdcAdapter, NAME);
+        if (!(tl instanceof TransmittableThreadLocal)) {
+            setFieldValue(mdcAdapter, NAME, new TransmittableThreadLocal<>());
+        }
+        put(TRACE_ID, traceId);
     }
 
     public static String getTraceId() {
-        return MDC.get(TRACE_ID);
+        return get(TRACE_ID);
     }
 
     public static void clearTraceId() {
-        MDC.remove(TRACE_ID);
+        remove(TRACE_ID);
     }
 }
