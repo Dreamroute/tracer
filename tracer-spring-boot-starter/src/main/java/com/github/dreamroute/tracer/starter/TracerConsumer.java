d2eba3d2d84c03f933430aa69cbf6691debc4361
wangdehai <wangdehai@yzw.cn>
2022-08-11 10:31:05 +0800

    fix: 处理npe

--- tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/TracerConsumer.java
+++ tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/TracerConsumer.java
@@ -1,5 +1,6 @@
 package com.github.dreamroute.tracer.starter;
 
+import lombok.Setter;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.Filter;
 import org.apache.dubbo.rpc.Invocation;
@@ -8,7 +9,6 @@ import org.apache.dubbo.rpc.Result;
 import org.apache.dubbo.rpc.RpcContext;
 import org.apache.dubbo.rpc.RpcException;
 
-import javax.annotation.Resource;
 import java.util.Set;
 
 import static com.github.dreamroute.tracer.starter.TracerProperties.TRACER;
@@ -22,16 +22,18 @@ import static org.apache.dubbo.common.constants.CommonConstants.CONSUMER;
 @Activate(group = CONSUMER)
 public class TracerConsumer implements Filter {
 
-    @Resource
-    private Tracer tracer;
+    @Setter
+    Tracer tracer;
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        Set<String> keys = tracer.getKeys();
-        if (keys != null && !keys.isEmpty()) {
-            for (String key : keys) {
-                String v = tracer.get(key);
-                RpcContext.getContext().setAttachment(TRACER + key, v);
+        if (tracer != null) {
+            Set<String> keys = tracer.getKeys();
+            if (keys != null && !keys.isEmpty()) {
+                for (String key : keys) {
+                    String v = tracer.get(key);
+                    RpcContext.getContext().setAttachment(TRACER + key, v);
+                }
             }
         }
         return invoker.invoke(invocation);
--- tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/TracerProvider.java
+++ tracer-spring-boot-starter/src/main/java/com/github/dreamroute/tracer/starter/TracerProvider.java
@@ -1,5 +1,6 @@
 package com.github.dreamroute.tracer.starter;
 
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.Filter;
@@ -9,7 +10,6 @@ import org.apache.dubbo.rpc.Result;
 import org.apache.dubbo.rpc.RpcContext;
 import org.apache.dubbo.rpc.RpcException;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,28 +25,33 @@ import static org.apache.dubbo.common.constants.CommonConstants.PROVIDER;
 @Activate(group = PROVIDER)
 public class TracerProvider implements Filter {
 
-    @Resource
-    private Tracer tracer;
+    @Setter
+    Tracer tracer;
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        Map<String, Object> attrs = RpcContext.getContext().getObjectAttachments();
-        if (attrs != null && !attrs.isEmpty()) {
-            Map<String, String> map = new HashMap<>();
-            attrs.forEach((k, v) -> {
-                if (k.startsWith(TRACER)) {
-                    map.put(k, (String) v);
+        if (tracer != null) {
+            Map<String, Object> attrs = RpcContext.getContext().getObjectAttachments();
+            if (attrs != null && !attrs.isEmpty()) {
+                Map<String, String> map = new HashMap<>();
+                attrs.forEach((k, v) -> {
+                    if (k.startsWith(TRACER)) {
+                        k = k.replace(TRACER, "");
+                        map.put(k, (String) v);
+                    }
+                });
+                if (!map.isEmpty()) {
+                    tracer.set(map);
                 }
-            });
-            if (!map.isEmpty()) {
-                tracer.set(map);
             }
         }
         Result result;
         try {
             result = invoker.invoke(invocation);
         } finally {
-            tracer.clear();
+            if (tracer != null) {
+                tracer.clear();
+            }
         }
         return result;
     }
