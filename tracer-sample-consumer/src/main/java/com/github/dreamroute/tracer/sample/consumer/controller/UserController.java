697652bdf3b3ecbe4800decc2fd9b7d8e90f1bc6
wangdehai <wangdehai@yzw.cn>
2022-10-27 19:21:16 +0800

    doc: 增加注释

--- tracer-sample-consumer/src/main/java/com/github/dreamroute/tracer/sample/consumer/controller/UserController.java
+++ tracer-sample-consumer/src/main/java/com/github/dreamroute/tracer/sample/consumer/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.github.dreamroute.tracer.sample.consumer.controller;
 
-import com.github.dreamroute.tracer.starter.Tracer;
 import com.github.dreamroute.tracer.sample.api.service.UserService;
+import com.github.dreamroute.tracer.starter.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
 
 @Slf4j
 @RestController
@@ -15,9 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-//    @Resource
-//    private ExecutorService pool;
-
+    // 如果不使用ttl，那么就是使用的是InheritableThreadLocal，下方的hotPool()不预热可能会看到正确的数据，但是一旦进行了预热，那么就无法传递参数了
+    private final ExecutorService pool;
     private final Tracer tracer;
 
     @DubboReference
@@ -25,22 +29,33 @@ public class UserController {
 
     @GetMapping("/index")
     public String index() {
+
+        hotPool();
+
         log.info("OUTER");
         userService.selectById(20L);
-//        pool.submit(() -> {
-//          log.info("INNER");
-//        });
-//
-//        Map<String, String> map = new HashMap<>();
-//        map.put("_name", "_w.dehai");
-//        tracer.set(map);
-//
-//        pool.submit(() -> {
-//            String name = MDC.get("_name");
-//            System.err.println(name);
-//        });
+        pool.submit(() -> {
+          log.info("INNER");
+        });
+
+        Map<String, String> map = new HashMap<>();
+        map.put("_name", "_w.dehai");
+        tracer.set(map);
+
+        pool.submit(() -> {
+            String name = MDC.get("_name");
+            System.err.println(name);
+        });
 
         return "ok";
+    }
+
+    /**
+     * 预热pool，避免请求到位了才创建线程
+     */
+    private void hotPool() {
+        for (int i=0; i<100; i++)
+            pool.submit(() -> {});
     }
 
 }
